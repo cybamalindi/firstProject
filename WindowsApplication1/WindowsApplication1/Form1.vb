c9491ad32959b83af2252f2f9f15480e913dc2e1
cybamalindi <cybamalindi@users.noreply.github.com>
2014-11-25 06:48:00 +0200

    Update Form1.vb
    tjho

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -2,5 +2,6 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ListBox1.Items.Add("hello world")
+        messagebox.show(":","cyba")
     End Sub
 End Class
